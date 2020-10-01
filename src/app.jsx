3debfa817b9b7dbdc1b14ec2f003400e1efcd747
jake_lyu <gomgom3409@gmail.com>
2020-10-01 13:24:27 +1000

    youtube_clone

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
+import { Grid } from '@material-ui/core';
 
-function App() {
-  return <h1>hello</h1>;
+import { SearchBar, VideoDetail, VideoList } from './components';
+import youtube from './api/youtube';
+
+// import youtube from './api/youtube';
+
+class App extends Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
+  componentDidMount() {
+    this.handleSubmit('motivation');
+  }
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: 5,
+        key: 'AIzaSyDdInymZMLdh4trTf5tRILg_br471ePaFQ',
+        q: searchTerm,
+      },
+    });
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+    // console.log(response.data.items.video.id)
+    console.log(response.data.items[0].id.videoId);
+  };
+
+  render() {
+    const { selectedVideo, videos } = this.state;
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail video={selectedVideo} />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
