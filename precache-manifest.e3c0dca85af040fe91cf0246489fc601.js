3b5a57be7cd19307413f7b6466a05d6550eddd6c
jake_lyu <gomgom3409@gmail.com>
2020-10-03 14:32:00 +1000

    Updates

---
+++ precache-manifest.e3c0dca85af040fe91cf0246489fc601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd0a6a2c1c7278e148d2d086706d68d",
+    "url": "/youtube_clone/index.html"
+  },
+  {
+    "revision": "b368e9826d141b136e3c",
+    "url": "/youtube_clone/static/css/main.e8009d42.chunk.css"
+  },
+  {
+    "revision": "6424f9b58ad896e83480",
+    "url": "/youtube_clone/static/js/2.b966ef8d.chunk.js"
+  },
+  {
+    "revision": "1cce617c8a34bcb8894f04e689935d99",
+    "url": "/youtube_clone/static/js/2.b966ef8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b368e9826d141b136e3c",
+    "url": "/youtube_clone/static/js/main.a4b07bb5.chunk.js"
+  },
+  {
+    "revision": "71eab35f46c64237e77e",
+    "url": "/youtube_clone/static/js/runtime-main.454c1147.js"
+  }
+]);
